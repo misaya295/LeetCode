3a7e010ec130a7c8638b1d8f99384b4fa8cda74b
chenwenkang <864730420@qq.com>
2020-05-15 16:41:27 +0800

    连续子数组 枚举

---
+++ src/main/java/day24/Solution.java
@@ -0,0 +1,32 @@
+package day24;
+
+
+/**
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ *
+ * 示例 1 :
+ *
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ *
+
+ */
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+}
