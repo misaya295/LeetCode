659037cd0f5f4b473659895ee3cafa501300c5f9
chenwenkang <864730420@qq.com>
2020-05-21 12:16:54 +0800

    回文数 dp

---
+++ src/main/java/one2thirty/day29/Solution.java
@@ -0,0 +1,53 @@
+package one2thirty.day29;
+
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    public String longestPalindrome(String s) {
+
+
+        if (s.equals("")) {
+            return "";
+        }
+        String origin = s;
+        String reverse = new StringBuffer(s).reverse().toString();
+        int length = s.length();
+        int[][] arr = new int[length][length];
+        int maxLen = 0;
+        int maxEnd = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (origin.charAt(i) == reverse.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = arr[i - 1][j - 1] + 1;
+                    }
+                }
+                if (arr[i][j] > maxLen) {
+                    maxLen = arr[i][j];
+                    maxEnd = i;
+                }
+            }
+        }
+
+        return s.substring(maxEnd - maxLen + 1, maxEnd + 1);
+
+    }
+}
