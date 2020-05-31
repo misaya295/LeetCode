a81c56081770c8f2ea5cd00d1805b52d157e07df
chenwenkang <864730420@qq.com>
2020-05-31 11:06:22 +0800

    对称二叉树 递归

---
+++ src/main/java/thitryontTOsixty/day32/Solution.java
@@ -0,0 +1,70 @@
+package thitryontTOsixty.day32;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *  
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *  
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *  
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+
+       return isMirror(root, root);
+
+
+        
+    }
+
+    private Boolean isMirror(TreeNode root1, TreeNode root2) {
+
+
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        return (root1.val == root2.val) && isMirror(root1.left,root2.right) && isMirror(root1.right,root2.left);
+
+
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}
