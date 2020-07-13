0e376ce1388a45c243015cbb296d0d82be3597d6
chenwenkang <864730420@qq.com>
2020-07-13 22:15:18 +0800

    集合 哈希

---
+++ src/main/java/leetcode/力扣题/题350/Solution.java
@@ -0,0 +1,55 @@
+package leetcode.力扣题.题350;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 350. 两个数组的交集 II
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ * 示例 2:
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ */
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
+        Map<Integer,Integer> map=new HashMap<Integer, Integer>();
+
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] insersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                insersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+
+        return Arrays.copyOfRange(insersection, 0, index
+        );
+
+    }
+}
